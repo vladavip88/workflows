78fcdcb9a0ec611a4e22d908a3bb1e9df48db99d
Vladimir Prsendic <vladavip88@gmail.com>
2016-02-14 13:39:15 +0100

    Coffe script

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util'); //alow logs in terminal
+var coffee = require('gulp-coffee');
 
-gulp.task('log',function(){
-	gutil.log('test')
+var coffeeSources = ['components/coffee/tagline.coffee']
+
+gulp.task('coffee',function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({ bare:true })
+			.on('error',gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
